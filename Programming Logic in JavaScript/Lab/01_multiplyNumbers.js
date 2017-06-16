b8d770054050476ae153ede40b8c3dfae9f1fea8
Deyan <dublinstand@gmail.com>
2017-06-16 14:15:05 +0100

    initial commit

---
+++ Programming Logic in JavaScript/Lab/01_multiplyNumbers.js
@@ -0,0 +1,9 @@
+//Write a JS function that calculates the product of two numbers.
+//    The input comes as array of strings, where each element holds a number.
+//    The output should be the returned as a result of your function.
+
+
+
+function multiplyNumbers([num1, num2]){
+
+}
